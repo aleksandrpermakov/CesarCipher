6656dc66e21cef7d14bc338aa8c07ff2d40c00d3
Alex <aleksandrpermakov105@gmail.com>
2025-04-25 13:38:56 +0500

    Добавьте файлы проекта.

---
+++ CesarCipher/CesarCipher.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CesarCipher
+{
+    internal class CesarCipher
+    {
+        string alphabet = string.Empty;
+        int key = 3;
+        public string Alphabet { get { return alphabet; } }
+        public CesarCipher()
+        {
+            for (char i = 'A'; i <= 'Z'; i++)
+            {
+                alphabet += i;
+            }
+        }
+        public char Cipher(char symbol)
+        {
+            char cipher_symbol = symbol;
+            for (int i = 0; i < alphabet.Length; i++)
+            {
+                if (symbol == alphabet[i])
+                {
+                    if(i < alphabet.Length - key) 
+                    {
+                        cipher_symbol = alphabet[i + key];
+                    }
+                    else
+                    {
+                        cipher_symbol = 
+                            alphabet[(i - alphabet.Length) + key];
+                    }
+                }
+            }
+            return cipher_symbol;
+        }
+        public char unCipher(char symbol)
+        {
+            char uncipher_symbol = symbol;
+            for (int i = 0; i < alphabet.Length; i++)
+            {
+                if (symbol == alphabet[i])
+                {
+                    if (i >= key)
+                    {
+                        uncipher_symbol = alphabet[i - key];
+                    }
+                    else
+                    {
+                        uncipher_symbol =
+                            alphabet[(i + alphabet.Length) - key];
+                    }
+                }
+            }
+            return uncipher_symbol;
+        }
+    }
+}
---
+++ CesarCipher/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CesarCipher
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CesarCipher cesarCipher = new CesarCipher();
+            Console.WriteLine(cesarCipher.Alphabet);
+            Console.WriteLine("Шифрование");
+            foreach (var symbol in cesarCipher.Alphabet)
+            {
+                Console.WriteLine($"{symbol} = {cesarCipher.Cipher(symbol)}");
+            }
+            Console.WriteLine("Дешифровка");
+            foreach (var symbol in cesarCipher.Alphabet)
+            {
+                Console.WriteLine($"{symbol} = {cesarCipher.unCipher(symbol)}");
+            }
+        }
+    }
+}
